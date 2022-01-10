7a960d4a05ac82cd0fb2698097bdcebcd07c8fd0
Phuc Tran <jamesptran96@gmail.com>
2022-01-10 12:56:47 -0800

    add env activation

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
+python3 -m venv ingestion_server
+source ingestion_server/bin/activate
+
 pip3 install -r requirements.txt
 echo "Starting flask server"
-echo "test jenkins"
+
 gunicorn --bind 0.0.0.0:8000 run:app

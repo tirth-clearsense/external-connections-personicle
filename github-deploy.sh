514248a23a52211d1c66c681457c07ea3d0737b6
Vaibhav Pandey <vaibhap1@uci.edu>
2022-02-07 17:00:54 -0800

    publish the branch to github

--- github-deploy.sh
+++
@@ -1,6 +0,0 @@
-# deploy the applications using github workflow and github secrets
-
-export CREDENTIALS_DB_USER=
-export CREDENTIALS_DB_PASSWORD=
-export CREDENTIALS_DB_HOST=
-export CREDENTIALS_DB_NAME=

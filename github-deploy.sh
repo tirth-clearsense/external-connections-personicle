f90c0ae371674110d3cc97e7fa82bbc2645e3318
Vaibhav Pandey <vaibhap1@uci.edu>
2022-02-07 16:52:25 -0800

    configured the app to use remote database for user tokens

---
+++ github-deploy.sh
@@ -0,0 +1,6 @@
+# deploy the applications using github workflow and github secrets
+
+export CREDENTIALS_DB_USER=
+export CREDENTIALS_DB_PASSWORD=
+export CREDENTIALS_DB_HOST=
+export CREDENTIALS_DB_NAME=
--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+# install postgres dev libraries before installing python dependencies
+sudo apt-get install libpq-dev python-dev
 pip3 install -r requirements.txt
 echo "Starting flask server"
 

d47463110efe592c928578f938121b7219f0fb52
Vaibhav Pandey <vaibhap1@uci.edu>
2022-02-08 22:28:16 -0800

    configuring for azure app service

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # install postgres dev libraries before installing python dependencies
 sudo apt-get install libpq-dev python-dev
-pip3 install -r requirements.txt
+# pip3 install -r requirements.txt
 echo "Starting flask server"
 
 gunicorn --bind 0.0.0.0:8000 run:app

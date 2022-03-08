52c1d0062290d8baca550329561283821e14632a
Vaibhav Pandey <vaibhap1@uci.edu>
2022-03-08 10:34:13 -0800

    logging changes in application setup

--- application/fitbit/__init__.py
+++ application/fitbit/__init__.py
@@ -1,4 +1,5 @@
 
+import traceback
 from flask.app import Flask
 from okta import UsersClient
 from flask_oidc import OpenIDConnect
@@ -7,7 +8,9 @@ def init_app(app: Flask):
     # register the models
     # create the tables
     # register blueprints
-
-    from .authorization_routes import fitbit_routes
-    app.register_blueprint(fitbit_routes)
-    
+    try:
+        from .authorization_routes import fitbit_routes
+        app.register_blueprint(fitbit_routes)
+    except Exception as e:
+        print(traceback.format_exc()) 
+        raise e   
